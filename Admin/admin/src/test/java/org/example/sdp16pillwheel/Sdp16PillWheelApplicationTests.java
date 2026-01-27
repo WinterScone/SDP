d3963a9c7afb39bf7865c51de352d7b59fcda6b6
Ting <edin0128burgh@gmail.com>
2026-01-27 14:50:27 +0000

    Add client and admin projects

---
+++ Admin/admin/src/test/java/org/example/sdp16pillwheel/Sdp16PillWheelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sdp16pillwheel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdp16PillWheelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
