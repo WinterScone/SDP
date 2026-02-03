411aafc7f7694c56f8ecafeee812bfa78575fd15
Ting <edin0128burgh@gmail.com>
2026-02-03 15:17:35 +0000

    gogogogo

--- Client/client/src/main/java/org/example/sdpclient/controller/AdminManagePatientDetailController.java
+++ Client/client/src/main/java/org/example/sdpclient/controller/AdminManagePatientDetailController.java
@@ -1,12 +1,12 @@
 package org.example.sdpclient.controller;
 
 import org.example.sdpclient.dto.*;
-
 import org.example.sdpclient.enums.MedicineType;
 import org.example.sdpclient.service.AdminManagePatientDetailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
 
 import java.util.List;
 
